4b43bbea3c9847895f14c34d2357fa9be8b800e6
KalokChan1370 <chankalok@hotmail.co.uk>
2021-12-14 22:42:34 +0000

    type check of parameters and arguments of function and callers

--- src/NewTypeChecker.py
+++ src/NewTypeChecker.py
@@ -1,43 +1,109 @@
 import ast
+import astpretty
 
-variables = []
+class vis(ast.NodeVisitor):
+    def __init__(self):
+        self.variables=[]
+        self.func_sign = {}
 
-# class vis(ast.NodeVisitor):
-#     def visit_Assign(self, node):
-#         for target in node.targets:
-#             print(target)
+    def visit_Assign(self, node):
+        if isinstance(node.value,ast.Call):
+            self.Call(node)
+        duplicateCheck(node, self.variables)   
 
-def duplicateCheck(new):
+    # assumes all variables used are local
+    def visit_FunctionDef(self, node):
+        sub_variables = []
+        annotations = []
+
+        print("variables in function:", node.name)
+
+        #requires source code to use type hints
+        for a in node.args.args:
+            if a.annotation !=None:
+                # eval not recommended
+                annotations.append((a.arg, eval(a.annotation.id)))
+        if node.returns != None:
+            annotations.append(('return type',eval(node.returns.id)))
+        self.func_sign[node.name]=annotations
+
+        for n in node.body:
+            if isinstance(n, ast.Assign):
+                duplicateCheck(n,sub_variables)
+        
+        print([x.targets[0].id for x in sub_variables])
+        print("-----"*10,"function def end","-----"*10,)
+
+    def Call(self, node):
+        arguments = node.value.args
+        print(f"-----"*10,node.value.func.id, "caller start","-----"*10)
+        parameters = self.func_sign[node.value.func.id]
+        count = 0
+        # for unassigned arguements ie not doing product(a=1, b=2)
+        if len(node.value.keywords) == 0:
+            for arg in arguments:
+                #TODO validate for return type 
+                if type(ast.literal_eval(arg))!= parameters[count][1] and parameters[count][0]!='return type':
+                    print(f"data type {type(ast.literal_eval(arg))} for argument at position '{count}' on line {arg.lineno} does not match the parameters of function: {node.value.func.id} ")
+                count+=1
+        else:
+            for key in node.value.keywords:
+                for pram in parameters:
+                    if key.arg == pram[0]:
+                        if type(ast.literal_eval(key.value)) != pram[1]:
+                            print(f"data type {type(ast.literal_eval(key.value))} for argument '{key.arg}' on line {node.lineno} does not match the parameters of function: {node.value.func.id}")
+        
+        #checks data type of return type
+        if parameters[-1][0]=='return type':
+            dup= False
+            name = node.targets[0].id
+            if len(self.variables)!=0:
+                for x in self.variables:
+                    if name == x.targets[0].id:
+                        dup = True 
+                        if isinstance(parameters[-1][1], type(x.value)):
+                            self.variables[name]=parameters[-1][1]
+                        else:
+                            print(f"return value data type for function call '{node.value.func.id}' does not match with current data type of '{name}'")
+                        break
+        print("-----"*10,"caller end","-----"*10,"\n")
+
+#TODO:Need to check about binary operations for assignments ie 
+#  a=b+c, a="hello" + 3, returns match
+#TODO: change how list is stored, dont need to store the whole node
+def duplicateCheck(new, variabelsList):
     dup = False
     name = new.targets[0].id
-    if len(variables) != 0:
-        for x in variables:
+    if len(variabelsList) != 0:
+        for x in variabelsList:
             if name == x.targets[0].id:
                 dup = True 
                 if isinstance(new.value, type(x.value)):
-                    print(f"data type for {new.targets[0].id} at {x.lineno} and at {new.lineno}: match")
+                    #updates variable position when type match
+                    variabelsList[variabelsList.index(x)]= new
                 else:
-                    print(f"data type for {new.targets[0].id} at {x.lineno} and at {new.lineno}: mismatch")
+                    print(f"data type for:'{new.targets[0].id}' at {x.lineno} and at {new.lineno}: mismatch")
                 break
         if not dup:
-            variables.append(new)
+            variabelsList.append(new)
     else:
-        variables.append(new)
+        variabelsList.append(new)
 
+
+# Program start
 with open("test.py",'r') as f:
     content = f.read()
     module = ast.parse(content)
     
-    # alternate method to find all assignments
-    # c=vis()
-    # c.visit(module)
-    # for x in variables:
-    #     print(x.value)
+    c=vis()
+      
     for node in module.body:
-        print(ast.dump(node))
-        if isinstance(node, ast.Assign):
-            
-            duplicateCheck(node)
+        c.visit(node)
 
-    print([x.targets[0].id for x in variables])
+    print("Global variables")
+    print([x.targets[0].id for x in c.variables])
+
+    print("Funcion signatures")
+    for k,v in c.func_sign.items():
+        print(k,":",v)
 
--- src/test.py
+++ src/test.py
@@ -1,10 +1,27 @@
+import math
+# def add (a,b):
+#     return a+b
+
+def trapezoidArea(a:int,b:int,h:int) ->int:
+    base = a+b
+    #base = 1
+    area = (base/2)*h
+    return area
+
+def stringSum(a:str, b:str = "yup", c:int =1) ->bool:
+    test= a+b
+    return len(test)
+area = "area"
+count = stringSum(a="hey", b="sum",c=[1,23])
+area = trapezoidArea(1,2,"hi")
+
 test = [2,3,4,5]
 test = [1]
 test = 1
 #test= "hey"
 
 calc = test + 2
-calc = 2.2 // 2
+calc = 2.2 * 2
 equal = "hello"
 equal== test
 
@@ -15,7 +32,8 @@ for i in range (0,12):
     hello = "hello"
     i+=1
 
-def add (a,b):
-    return a+b
 
-total= add(1,2)
+
+
+# total= sum([1,2])
+# sqrt= math.sqrt(2)
